91e13334fbd2db401ec567c718eb9988286e46cb
Mishel <mcarrionl@uni.pe>
2019-11-10 01:09:03 -0500

    frontend

--- js/main.js
+++ js/main.js
@@ -62,4 +62,30 @@ $(document).ready(function(){
 	    }  // end of success fn
 	   }); // end of Ajax call
 	   */
+	
+	$(".sugerencia").click(function(){
+		window.location.href = "book.html"
+	});
+
+	
+	$("#s1").click(function(){
+		$(".fa-star").css("color", "black");
+		$("#s1").css("color", "yellow");
+	});
+	$("#s2").click(function(){
+		$(".fa-star").css("color", "black");
+		$("#s1, #s2").css("color", "yellow");
+	});
+	$("#s3").click(function(){
+		$(".fa-star").css("color", "black");
+		$("#s1,#s2,#s3").css("color", "yellow");
+	});
+	$("#s4").click(function(){
+		$(".fa-star").css("color", "black");
+		$("#s1,#s2,#s3,#s4").css("color", "yellow");
+	});
+	$("#s5").click(function(){
+		$(".fa-star").css("color", "black");
+		$(".fa-star").css("color", "yellow");
+	});
 });
